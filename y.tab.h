9f30b566473dc8b51541b884e71f762376f8f2fa
EncarnacionJonathanRojasMayo <38919709+EncarnacionJonathanRojasMayo@users.noreply.github.com>
2018-05-21 15:33:14 -0500

    y.tab.h DELIMITADORES
    CODIFICO: ROJAS MAYO ENCARNACION JONATHAN
    DOCUMENTO: LUNA ESCALONA MIGUEL YOSHIMAR
    INTEGRO: ROSALES GONZALEZ CARLOS ANDRES
    
    SE DEFINIERON LAS VARIABLES PARA IDENTICADORES, STRING, INT, DISPLAY(DESPLEJAR), Y PARA ACEPTAR LAS CONDICIONES IF ELSE

--- y.tab.h
+++ y.tab.h
@@ -4,10 +4,10 @@ typedef union {
    string sval;
 } YYSTYPE;
 #define	ID	258
-#define	STRING	259
-#define	INT	260
-#define	DESPLIEGA 261
-#define SI        262
-#define OTRO      263
-#define ENTERO    269
+#define	STRING   259
+#define	INT   260
+#define	DISPLAY  261
+#define IF  262
+#define ELSE   263
+#define INT 269
 extern YYSTYPE yylval;
